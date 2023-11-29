fd8de1fce1306aa0ea657e633b951e72d5fd10f4
Moontrick <47784395+Moontrick@users.noreply.github.com>
2023-11-29 18:32:34 +0400

    add new

--- src/pages/ResultPhoto/ResultPhotoPage.js
+++ src/pages/ResultPhoto/ResultPhotoPage.js
@@ -25,17 +25,21 @@ function ResultPhotoPage() {
         <div className='Photo_center' >
         {users_result_photo && users_result_photo.map((photo, index) => (
             <div key={photo.user_photo}>
-        
+        {/* Коэффициент K - индекс распластанности переднего отдела стопы */}
+        {/* Коэффициент I - индекс Штритера */}
+        {/* Коэффициент Альфа  - отклонение первого пальца*/}
+        {/*  */}
         <div className='vw-100 app_up_block_Photo'>
         <h1 className='h1'>Результат исследований</h1>
             <h3 style={{fontSize:"30px"}}>Дата загрузки исследования: &nbsp;<span> {photo.date_upload};</span></h3>
             <h3 style={{fontSize:"30px"}}>Итоговый результат: &nbsp;<span> {photo.preview_result};</span></h3>
-            <h3 style={{fontSize:"30px", lineClamp:"3"}}>Коэффициент общий: &nbsp;<span>{photo.coeficientcommont};</span></h3>
-            <h3 style={{fontSize:"30px"}}>Коэффициент K: &nbsp;<span>{photo.coeficientK};</span></h3>
-            <h3 style={{fontSize:"30px"}}> Коэффициент I: &nbsp;<span> {photo.coeficientI};</span></h3>
+            <h3 style={{fontSize:"30px", lineClamp:"3"}}>Коэффициент продольного плоскостопия: &nbsp;<span>{photo.coeficientcommont};</span></h3>
+            <h3 style={{fontSize:"30px"}}>Коэффициент распластанности верхнего отдела стопы: &nbsp;<span>{photo.coeficientK};</span></h3>
+            <h3 style={{fontSize:"30px"}}> Индекс Вальгуса: &nbsp;<span> {photo.coefficient_alpha};</span></h3>
+            <h3 style={{fontSize:"30px"}}> Индекс Штритера : &nbsp;<span> {photo.coeficientI};</span></h3>
             <h3 style={{fontSize:"30px"}}>Пояснения:</h3>
             <h3 style={{fontSize:"30px"}}> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{photo.result_view}</span></h3>
-            <h3 style={{fontSize:"30px"}}>Номер и Код исследования для поиска: &nbsp;<span style={{color:"black"}}>{photo.photo_pers_identifier}</span>#<span style={{color:"black"}}>{photo.photo_secure_number}</span></h3>
+            <h3 style={{fontSize:"25px"}}>Номер и Код исследования для поиска: &nbsp;<span style={{color:"black"}}>{photo.photo_pers_identifier}</span>#<span style={{color:"black"}}>{photo.photo_secure_number}</span></h3>
             <h1 className='h1 mt-5'>Снимки результатов</h1>
             </div >
             <div className='Profile_Result_div mb-5'>
@@ -56,7 +60,7 @@ function ResultPhotoPage() {
              <img className='Profile_Result_image'
               src={`http://localhost:8000${photo.user_photo_third}`}
             />
-            <p style={{fontSize:"3vh", color:"white"}}>Индекс Вальгуса</p>
+            <p style={{fontSize:"3vh", color:"white"}}>Продольное плоскостопие</p>
             </div>
             </div>
             </div>
